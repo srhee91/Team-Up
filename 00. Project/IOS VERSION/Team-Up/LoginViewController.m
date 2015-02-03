893a5b815884c838fa0884dc673a3fa2e796f8ca
jeongb <jeongb@purdue.edu>
2015-02-03 12:56:05 -0500

    Checking Log In ID and PW successful

--- 00. Project/IOS VERSION/Team-Up/LoginViewController.m
+++ 00. Project/IOS VERSION/Team-Up/LoginViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LoginViewController.h"
+#import "Parse/parse.h"
 
 @interface LoginViewController ()
 
@@ -48,6 +49,29 @@
     UITapGestureRecognizer * tap= [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
     
     [self.view addGestureRecognizer:tap];
+    
+}
+-(IBAction)signin:(id)sender{
+    PFUser *user = [PFUser user];
+    NSString *username = self.username.text;
+    NSString *password = self.password.text;
+    if(username==nil || password == nil){
+        NSLog(@"Missing information");
+    }
+    else{
+        user.username = username;
+        user.password = password;
+        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser *user, NSError *error) {
+            if(user){
+                NSLog(@"login successful");
+                // Now application displays the MyGroup Page
+            }
+            else{
+                // Ask for re-input of either username or password
+            }
+        }];
+
+    }
 }
 
 -(void) dismissKeyboard {
