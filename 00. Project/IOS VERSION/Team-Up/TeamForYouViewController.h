192276dc12ba931bcc0e968360393a56404959d1
YIYANG PAN <yiyangpan@YIYANGs-Mac.local>
2015-03-10 20:00:39 -0700

    Create TeamForYouViewController to list all the user preferences

---
+++ 00. Project/IOS VERSION/Team-Up/TeamForYouViewController.h
@@ -0,0 +1,22 @@
+//
+//  TeamForYouViewController.h
+//  Team-Up
+//
+//  Created by YIYANG PAN on 3/10/15.
+//  Copyright (c) 2015 Kartik Sawant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TeamForYouViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *un;
+@property (weak, nonatomic) IBOutlet UILabel *em;
+@property (weak, nonatomic) IBOutlet UILabel *bd;
+@property (weak, nonatomic) IBOutlet UILabel *des;
+@property (weak, nonatomic) IBOutlet UITableView *tv;
+@property (weak, nonatomic) IBOutlet UINavigationItem *navbar;
+@property (strong, nonatomic) NSArray *array;
+@property (strong, nonatomic) NSArray *gId;
+
+@end
