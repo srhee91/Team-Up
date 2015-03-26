0914d0573bc82e74e63d3c71c455c1d1be4aa346
jeongb <jeongb@purdue.edu>
2015-03-26 03:58:29 -0400

    Changing Admin and Kicking Member Functionalities implemented
    Missing pop-ups

---
+++ 00. Project/IOS VERSION/Team-Up/AdminPopupViewController.h
@@ -0,0 +1,14 @@
+//
+//  AdminPopupViewController.h
+//  Team-Up
+//
+//  Created by Bo heon Jeong on 3/26/15.
+//  Copyright (c) 2015 Kartik Sawant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdminPopupViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *okButton;
+
+@end
